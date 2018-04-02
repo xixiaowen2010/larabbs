b18c57c12e4e16fb8235f8d6b182b1d449fbe33d
vagrant <vagrant@homestead>
2018-04-02 05:33:38 +0000

    category topic list

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,1 +1,4 @@
+
+<div class="panel panel-default panel-heading">
 右边导航栏
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,4 @@ Route::resource('users', 'UserController', ['only' => ['show', 'update', 'edit']
 //Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
