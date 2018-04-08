b688e7ff84526c40e3145460c2919fb8cb762f49
vagrant <vagrant@homestead>
2018-04-08 05:51:18 +0000

    Admin can manage site settings

--- config/administrator/topics.php
+++ config/administrator/topics.php
@@ -17,7 +17,7 @@ return [
             'sortable' => false,
             'output'   => function ($value, $model) {
 //                return '<div style="max-width:260px">' . model_link($value, $model) . '</div>';
-                return '<div style="max-width:260px">' . model_admin_link(e($model->topic->title), $model->topic) . '</div>';
+                return '<div style="max-width:260px">' . model_admin_link(e($model->title), $model) . '</div>';
             },
         ],
         'user' => [
