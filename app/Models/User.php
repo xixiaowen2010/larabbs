1d0658436ee03e5b1760b64a5a56cb256d5ccd9e
vagrant <vagrant@homestead>
2018-04-10 03:18:16 +0000

    keep track of user last visited time

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
     use HasRoles;
     use Notifiable{
         notify as protected laravelNotify;
