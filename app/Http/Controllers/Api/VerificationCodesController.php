71003976c70988c24d933f7fd8c558d4370c0216
vagrant <vagrant@homestead>
2018-04-16 06:24:24 +0000

    add socialiteproviders

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -10,17 +10,29 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
         if (!app()->environment('production')) {
             $code = '1234';
         } else {
-
             // 生成4位随机数，左侧补0
             $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
             try {
                 $result = $easySms->send($phone, [
-                    'content' => "【wedeal为商】您的验证码是{$code}。打死也不告诉别人。"
+                    'content'  =>  "【Lbbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
                 ]);
             } catch (\GuzzleHttp\Exception\ClientException $exception) {
                 $response = $exception->getResponse();
@@ -28,10 +40,13 @@ class VerificationCodesController extends Controller
                 return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
             }
         }
+
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -14,7 +14,17 @@ class VerificationCodeRequest extends FormRequest
     public function rules()
     {
         return [
-            'phone' => 'required|regex:/^1[34578]\d{9}$/|unique:users',
+//            'phone' => 'required|regex:/^1[34578]\d{9}$/|unique:users',
+            'captcha_key' => 'required|string',
+            'captcha_code' => 'required|string',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'captcha_key' => '图片验证码 key',
+            'captcha_code' => '图片验证码',
         ];
     }
 }
